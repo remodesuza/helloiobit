85435972faab76e3a3e4fc09192c33134563ca46
remodesuza <remodesuza@proton.me>
2026-01-17 22:03:42 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "IObit Advanced SystemCare Ultimate Pro Download: Complete Installation & Activation Tutorial"
+html_title = "Download IObit Malware Fighter: Latest Version & IObit Malware Fighter 12 Pro Download Guide"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
